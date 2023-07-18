d6b35c59e4fab4a72251df16e98b3b2f193475e1
kenminsoo <minsookatsu@gmail.com>
2023-07-17 18:00:44 -1000

    install-testubuntu

--- install/easy_install.sh
+++ install/easy_install.sh
@@ -11,11 +11,17 @@ eval "$(conda shell.bash hook)"
 conda create --name sRNAfrag --file requirements_conda.txt
 conda activate sRNAfrag
 
+conda install -c bioconda -y bowtie=1.3.1
+conda install -c bioconda -y bowtie2=2.5.1
+conda install -c bioconda -y umi_tools=1.1.4
+conda install -c bioconda -y adapterremoval=2.3.3
+conda install -c bioconda -y bedtools=2.30.0
+conda install -c bioconda -y fastqc=0.12.1
+conda install -c bioconda -y fastx_toolkit=0.0.14
+conda install -c bioconda -y cutadapt=4.4
+conda install -c bioconda -y hisat2=2.2.1
+conda install -c bioconda -y bedtools=2.30.0
+
 echo "conda installable packages, installed"
-
-# Pip installable packages
-pip install -r requirements.txt
-
-echo "pip installable packages, installed"
 
 pwd > install_wd.txt
